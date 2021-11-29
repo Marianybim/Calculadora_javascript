edf3abac6bbdf8568b15bfdebb6546be857c08ae
Mariany Bim <79792549+Marianybim@users.noreply.github.com>
2021-11-29 14:57:25 -0300

    Add files via upload

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,8 +6,8 @@ function limpar(){
 	document.getElementById('resultado').innerHTML ="";
 }
 function voltar(){
-	var resultado = document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length-1);
-
+	var resultado = document.getElementById('resultado') .innerHTML;
+	document.getElementById('resultado').innerHTML = resultado .substring(0,resultado.length -1)
 }
 function calcular(){
 	var resultado = document.getElementById('resultado').innerHTML;
